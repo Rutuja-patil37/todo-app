7ca87c4d35c29dd24abf988d3968621ae1c6efa4
Rutuja-patil37 <03rutuja@gmail.com>
2026-02-18 19:08:39 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./App.css";
+function addTodo() {
+  const input = document.getElementById("todoInput");
+  const list = document.getElementById("todoList");
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  if (input.value === "") return;
+
+  const li = document.createElement("li");
+  li.textContent = input.value;
+
+  list.appendChild(li);
+  input.value = "";
+}
